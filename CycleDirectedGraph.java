563515cb30047926160e0c80ed97069d63250de4
rohan.patil2 <prorohan8@gmail.com>
2022-09-12 15:21:21 +0530

    graph problems

--- CycleDirectedGraph.java
+++ CycleDirectedGraph.java
@@ -1,28 +1,62 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class CycleDirectedGraph {
 
 
-    public boolean detectDFS(int src,ArrayList<ArrayList<Integer>> adj,int[] visited,int[] pathVisited){
+    //Using TopoSort
+    public static boolean detectBFS(int V, ArrayList<ArrayList<Integer>> adj) {
+        //If topo comes out <N then it has cycle
+        Queue<Integer> q = new LinkedList<>();
 
-        visited[src]  = 1;
+        int[] indegree = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (int k : adj.get(i)) {
+                indegree[k]++;
+            }
+        }
+
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) q.add(i);
+        }
+
+        int[] topoArr = new int[V];
+        int i = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            topoArr[i++] = node;
+            for (int k : adj.get(node)) {
+                indegree[k]--;
+                if (indegree[k] == 0) q.add(k);
+            }
+        }
+
+        //If matches then No Cycle
+        //If not matches then Cycle
+        return !(topoArr.length == V);
+    }
+
+    public boolean detectDFS(int src, ArrayList<ArrayList<Integer>> adj, int[] visited, int[] pathVisited) {
+
+        visited[src] = 1;
         pathVisited[src] = 1;
 
         //Traverse All Nodes
-        for(int k: adj.get(src)){
+        for (int k : adj.get(src)) {
 
             //If Not Visited
-            if(visited[k]==0){
+            if (visited[k] == 0) {
 
-                if(detectDFS(k,adj,visited,pathVisited)){
+                if (detectDFS(k, adj, visited, pathVisited)) {
                     return true;
                 }
-            }else{
+            } else {
 
                 //If the node is previously visited
 
                 //visited + pathVisited
-                if(pathVisited[k]==1){
+                if (pathVisited[k] == 1) {
                     return true;
                 }
 
@@ -43,9 +77,9 @@ public class CycleDirectedGraph {
         int[] pathVisited = new int[V];
 
 
-        for(int i=0;i<V;i++){
-            if(visited[i]==0){
-                if(detectDFS(i,adj,visited,pathVisited)) return true;
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == 0) {
+                if (detectDFS(i, adj, visited, pathVisited)) return true;
             }
         }
 
--- TopologicalSort.java
+++ TopologicalSort.java
@@ -1,7 +1,46 @@
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class TopologicalSort {
+    static int[] topoBFS(int V, ArrayList<ArrayList<Integer>> adj){
+        Queue<Integer> q = new LinkedList<>();
+
+        int[] indegree = new int[V];
+        ///Get Indegrees
+        for(int i=0;i<V;i++){
+            for(int k:adj.get(i)){
+                indegree[k]++;
+            }
+        }
+
+        ///Add all with 0 to the queue
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+
+        }
+        int[] topoArr = new int[V];
+        int i=0;
+        ///Add all with 0 to topoL
+        while(!q.isEmpty()){
+            int node = q.poll();
+            topoArr[i++] = node;
+
+            for(int k: adj.get(node)){
+                indegree[k]--;
+                if(indegree[k]==0){
+                    q.add(k);
+                }
+            }
+        }
+        return topoArr;
+
+    }
+
+
     static void topoDFS(int src, ArrayList<ArrayList<Integer>> adj,Stack<Integer> stack,int[] visited){
         visited[src] = 1;
         for(int k : adj.get(src)){
