d4dab305812c529e4c3dc952aef245726efd0dba
rohan.patil2 <prorohan8@gmail.com>
2022-09-21 19:58:30 +0530

    vertical tree traversal

--- VerticalTreeTraversal.java
+++ VerticalTreeTraversal.java
@@ -1,67 +1,67 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
-class VPair{
+class VPair {
     BNode node;
-    int vl;
-    public VPair(BNode node, int vl){
+    int x;
+    int y;
+
+    public VPair(BNode node, int x, int y) {
         this.node = node;
-        this.vl = vl;
+        this.x = x;
+        this.y = y;
     }
 }
+
+class VPairComparator implements Comparator<VPair> {
+    @Override
+    public int compare(VPair o1, VPair o2) {
+        if (o1.y == o2.y) {
+            return o1.node.data - o2.node.data;
+        }
+
+        return o1.y - o2.y;
+    }
+}
+
 public class VerticalTreeTraversal {
 
-    static ArrayList<Integer> verticalOrder(BNode root)
-    {
-        // add your code here
-        LinkedList<VPair> que = new LinkedList<>();
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        int minVl = 0,maxVl = 0;
-        // -2 -> 4  -1->2 0->1,5
-        List<List<Integer>> ans = new ArrayList<>();
+    static List<List<Integer>> verticalOrder(BNode root) {
+        List<List<Integer>> res = new ArrayList<>();
 
 
+        Map<Integer, PriorityQueue<VPair>> map = new HashMap<>();
+        Queue<VPair> q = new LinkedList<>();
+        q.add(new VPair(root, 0, 0));
 
-        que.addLast(new VPair(root,0));
-        while(!que.isEmpty()){
+        int leftMin = 0, rightMax = 0;
+        while (!q.isEmpty()) {
+            VPair point = q.poll();
 
-            int size = que.size();
-            while(size-- > 0){
-                VPair pair = que.removeFirst();
+            root = point.node;
+            map.putIfAbsent(point.x, new PriorityQueue<>(new VPairComparator()));
+            map.get(point.x).add(point);
+            leftMin = Math.min(leftMin, point.x);
+            rightMax = Math.max(rightMax, point.y);
 
-                maxVl = Math.max(maxVl,pair.vl);
-                minVl = Math.max(minVl,pair.vl);
-
-
-                map.putIfAbsent(pair.vl,new ArrayList<>());
-
-                map.get(pair.vl).add(pair.node.data);
-
-                if(pair.node.left != null)
-                    que.addLast(new VPair(pair.node.left,pair.vl-1));
-
-                if(pair.node.right != null)
-                    que.addLast(new VPair(pair.node.right,pair.vl+1));
-
+            if (root.left != null) {
+                q.add(new VPair(root.left, point.x - 1, point.y + 1));
             }
-
+            if (root.right != null) {
+                q.add(new VPair(root.right, point.x + 1, point.y + 1));
+            }
         }
 
-        for(int i=minVl; i<=maxVl;i++){
-            ans.add(map.get(i));
+        //Prepare ans
+        for (int i = leftMin; i <= rightMax; i++) {
+            PriorityQueue<VPair> pq = map.get(i);
+            List<Integer> list = new ArrayList<>();
+            while (!pq.isEmpty()) {
+                list.add(pq.poll().node.data);
+            }
+            res.add(list);
         }
 
-        //-----------Prepare output
-        ArrayList<Integer> finalAns = new ArrayList<>();
-
-        for(List<Integer> list : ans){
-            finalAns.addAll(list);
-        }
-
-
-        return finalAns;
+        return res;
     }
 }
