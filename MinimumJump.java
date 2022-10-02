4a73a9b1b70d8127939b26232f081a682b662ae9
rohan.patil2 <prorohan8@gmail.com>
2022-10-02 15:06:59 +0530

    prblems

--- MinimumJump.java
+++ MinimumJump.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MinimumJump {
 
 
@@ -18,24 +20,52 @@ public class MinimumJump {
                 return jump;
             }
             reachable = Math.max(reachable, arr[i] + i);
-            System.out.println("i: "+i+"Reachable: " + reachable + " Steps: " + step);
+//            System.out.println("i: " + i + "Reachable: " + reachable + " Steps: " + step);
             step--;
             if (step == 0) {
                 jump++;
                 if (i >= reachable) {
-                    System.out.println("NOt Reachable : "+reachable +" i:"+i);
+//                    System.out.println("NOt Reachable : " + reachable + " i:" + i);
                     return -1;
                 }
                 step = reachable - i;
-                System.out.println("Jump: " + jump + " Steps: " + step);
+//                System.out.println("Jump: " + jump + " Steps: " + step);
             }
         }
 
         return jump;
     }
 
+    public static int minimumJumpDP(int[] arr) {
+        int n = arr.length;
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+
+
+        for (int i = 1; i < n; i++) {
+            System.out.println("i :"+i);
+            for (int j = 0; j < i; j++) {
+                System.out.println("j: "+j+" canReachJ: "+(arr[j] + j));
+                boolean canReach = arr[j] + j >= i;
+                if (dp[j] != Integer.MAX_VALUE && canReach) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+
+            }
+        }
+
+        WordWrapDP.printArr(dp);
+        if (dp[n - 1] == Integer.MAX_VALUE) return -1;
+        return dp[n - 1];
+    }
+
+
     public static void main(String[] args) {
         System.out.println(minimumJump(new int[]{2, 3, 1, 1, 0}));
+        System.out.println(minimumJumpDP(new int[]{1,4,3,2,6,7}));
     }
 
 }
