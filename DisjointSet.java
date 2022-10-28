a6d36f8c55b6435639f17cde3d50507b41de77a9
rohan.patil2 <prorohan8@gmail.com>
2022-10-28 08:46:08 +0530

    disjoint set impl

--- DisjointSet.java
+++ DisjointSet.java
@@ -1,53 +1,72 @@
 public class DisjointSet {
 
     static int[] parent = new int[8];
-    static int[] height = new int[8];
+    static int[] rank = new int[8];
+    static int[] size = new int[8];
 
 
     public static void createSet(int n) {
         //Every parent is himself & height 0
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
-            height[i] = 0;
+            rank[i] = 0;
         }
     }
-
 
     public static int findParent(int node) {
         if (node == parent[node]) {
             return node;
         }
 
-        return parent[node] = node;
+        return parent[node] = findParent(parent[node]);
     }
 
 
-    public static void union (int node1,int node2) {
-        node1 = findParent(node1);
-        node2 = findParent(node2);
+    public static void union(int node1, int node2) {
+        int node1UP = findParent(node1); //ultimate parent of node1
+        int node2UP = findParent(node2); //ultimate parent of node2
 
-        if(height[node1] < height[node2]){
+        if (node1UP == node2UP) return;
+
+        if (rank[node1UP] < rank[node2UP]) {
             //node1->node2
-            parent[node1] = node2;
-        }else if(height[node1] > height[node2]){
+            parent[node1UP] = node2UP;
+        } else if (rank[node1UP] > rank[node2UP]) {
             //node2->node1
-            parent[node2] = node1;
-        }else{
-            //== height so any
-            parent[node2] = node1;
-            height[node1] +=1;
+            parent[node2UP] = node1UP;
+        } else {
+            //== rank so any
+            parent[node2UP] = node1UP;
+            rank[node1UP] += 1;
+        }
+    }
+
+    public static void unionBySize(int node1, int node2) {
+        int node1UP = findParent(node1); //ultimate parent of node1
+        int node2UP = findParent(node2); //ultimate parent of node2
+
+        if (node1UP == node2UP) return;
+
+        if (size[node1UP] < size[node2UP]) {
+            //node1->node2
+            parent[node1UP] = node2UP;
+            size[node2UP] += size[node1UP];
+        } else { //both cases combined
+            //node2->node1
+            parent[node2UP] = node1UP;
+            size[node1UP] += size[node2UP];
         }
     }
 
     public static void main(String[] args) {
         createSet(7);
-        union(1,2);
-        union(2,3);
-        union(4,5);
-        union(6,7);
-        union(5,6);
-        union(3,7);
-        for(int x:height){
+        union(1, 2);
+        union(2, 3);
+        union(4, 5);
+        union(6, 7);
+        union(5, 6);
+        union(3, 7);
+        for (int x : rank) {
             System.out.println(x);
         }
     }
