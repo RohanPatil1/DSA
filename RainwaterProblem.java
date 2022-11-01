da307e37e3824354e3e481813e801e0fcdc664ab
rohan.patil2 <prorohan8@gmail.com>
2022-11-01 18:54:01 +0530

    trapping rainwater 2ptr approach added

--- RainwaterProblem.java
+++ RainwaterProblem.java
@@ -61,6 +61,37 @@ public class RainwaterProblem {
         return water;
     }
 
+    public int trap(int[] height) {
+
+
+        int left =0 ,right =height.length-1;
+        int leftMax =height[left], rightMax = height[right];
+
+        int water = 0;
+
+
+        while(left<right){
+
+            if(leftMax < rightMax){
+                left++;
+                leftMax = Math.max(leftMax , height[left]);
+
+                water += leftMax - height[left];
+
+            }else{
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+
+                water += rightMax - height[right];
+            }
+
+
+        }
+
+
+        return water;
+    }
+
 
     public static void main(String[] args) {
 
